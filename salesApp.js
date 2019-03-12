e3c43d339db56792cb8480cfc397fb603bdfdc87
kcerbulis <kcerbulis@gmail.com>
2019-03-12 15:23:32 -0700

    started work on tables

---
+++ salesApp.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var table_headers = [' ', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+var row1 = pikeTimeAndCookieArray;
+// console.log(pikeTimeAndCookieArray);
+
+var row2 = seatacTimeAndCookieArray; 
+console.log(seatacTimeAndCookieArray);
+
+var row3 = centerTimeAndCookieArray;
+console.log(centerTimeAndCookieArray);
